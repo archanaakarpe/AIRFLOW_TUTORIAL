cb7f2056e54b9eea88ce6664f35dbe4cc261c721
Archana Karpe <43923595+archanaakarpe@users.noreply.github.com>
2023-02-03 14:41:54 +0700

    Add files via upload

---
+++ dag_with_cron_expression.py
@@ -0,0 +1,24 @@
+from datetime import datetime, timedelta
+
+from airflow import DAG
+from airflow.operators.bash import BashOperator
+
+
+default_args = {
+    'owner' : 'Archana',
+    'retries' : 5,
+    'retry_delay' : timedelta(minutes=5)
+}
+
+with DAG(
+    dag_id="dag_with_cron_expression_v01",
+    default_args=default_args,
+    start_date=datetime(2022, 11, 1),
+    schedule_interval='@daily'
+) as dag:
+
+    task1 = BashOperator(
+        task_id='first_task',
+        bash_command="echo dag with cron expression!"
+    )
+    task1
